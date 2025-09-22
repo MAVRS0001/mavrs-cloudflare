0fe322aa31eb81122444fe5349cdd3d31d668b1c
Your Name <your@email.com>
2025-09-22 16:22:33 +0300

    Improve diag: show present/nonEmpty/length

--- functions/api/debug/diag.js
+++ functions/api/debug/diag.js
@@ -1,6 +1,25 @@
-export const onRequestGet = async ({ env }) =>
-  new Response(JSON.stringify({
-    has_TELNYX_API_KEY: !!env.TELNYX_API_KEY,
-    has_TELNYX_MSG_PROFILE: !!env.TELNYX_MESSAGING_PROFILE_ID,
-    has_TELNYX_FROM_NUMBER: !!env.TELNYX_FROM_NUMBER,
-  }), { headers: { "content-type":"application/json", "Access-Control-Allow-Origin":"*" }});
+export const onRequestGet = async ({ env }) => {
+  const keys = [
+    "TELNYX_API_KEY",
+    "TELNYX_MESSAGING_PROFILE_ID",
+    "TELNYX_FROM_NUMBER",
+    "GMAPS_GEOCODING_KEY"
+  ];
+  const report = {};
+  for (const k of keys) {
+    const present = Object.prototype.hasOwnProperty.call(env, k);
+    const value = present ? String(env[k] ?? "") : "";
+    report[k] = {
+      present,
+      nonEmpty: present && value.trim().length > 0,
+      length: value.length
+    };
+  }
+  return new Response(JSON.stringify(report, null, 2), {
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "no-store",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+};
